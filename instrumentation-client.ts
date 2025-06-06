d084f3e3da94377fdcfd1a9f6e5c21dd3d55ef66
Subransu <maharanasubhransu6@gmail.com>
2025-06-06 17:36:35 +0530

    Update the Sentry code

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://0a64b8300136f7cdb59676fad0bb3935@o4509427528499200.ingest.us.sentry.io/4509451537219584",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Add optional integrations for additional features
   integrations: [
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://0a64b8300136f7cdb59676fad0bb3935@o4509427528499200.ingest.us.sentry.io/4509451537219584",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
